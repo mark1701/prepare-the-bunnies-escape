b0196a3fa41168b4258cbb66c1b9c4e036cf5bfb
mark1701 <mp@adelard.com>
2020-01-01 17:48:24 +0000

    various typos fixed plus refactoring

--- solution.py
+++ solution.py
@@ -1,17 +1,17 @@
 import sys
 import time
 
-MAX_INT = 2**31-1
+MAX_INT = 2 ** 31 - 1
+
 
 def create_weights_matrix(matrix):
-    # The original matrix contains 0 for an empty cell and 1 for a walled one.
-    # We change them to positive numbers 1 and 1000 respectively.
-    # This allows to calculate shortest paths based on the sum of the weights
-    # preferring the 1 options always (since the mazes used by the google foo bar challenge are max 20 in size and the sum of empty cells
-    # will never exceed 1000).
+    # The original matrix contains 0 for an empty cell and 1 for a walled one. We change them to positive numbers 1
+    # and 1000 respectively. This allows to calculate shortest paths based on the sum of the weights preferring the 1
+    # options always (since the mazes used by the google foo bar challenge are max 20 in size and the sum of empty
+    # cells will never exceed 1000).
 
-    EMPTY_CELL_WEIGHT = 1
-    WALLED_CELL_WEIGHT = 1000
+    empty_cell_weight = 1
+    walled_cell_weight = 1000
 
     number_rows = len(matrix)
     number_columns = len(matrix[0])
@@ -21,9 +21,9 @@ def create_weights_matrix(matrix):
     for i in range(0, number_rows):
         for j in range(0, number_columns):
             if matrix[i][j] == 0:
-                weights_matrix[i][j] = EMPTY_CELL_WEIGHT
+                weights_matrix[i][j] = empty_cell_weight
             else:
-                weights_matrix[i][j] = WALLED_CELL_WEIGHT
+                weights_matrix[i][j] = walled_cell_weight
 
     return weights_matrix
 
@@ -31,9 +31,8 @@ def create_weights_matrix(matrix):
 def find_adjacents_of_a_cell(x, y, number_columns, number_rows):
     # Simply returns an array of coordinates of cells
     # that are adjacent to a given one passed as parameter (x,y).
-    # Also requires the size of the containg matrix to take bonduaries into consideration.
+    # Also requires the size of the containing matrix to take boundaries into consideration.
 
-    
     if x >= number_rows or y >= number_columns:
         return []
 
@@ -83,25 +82,25 @@ def BFS(matrix):
 
     while len(queue) > 0:
         node = queue.pop(0)
-        nodeX = node[0]
-        nodeY = node[1]
+        node_x = node[0]
+        node_y = node[1]
 
-        visited_matrix[nodeX][nodeY] = 1
+        visited_matrix[node_x][node_y] = 1
 
-        adjacents = find_adjacents_of_a_cell(nodeX, nodeY, cols, rows)
+        adjacents = find_adjacents_of_a_cell(node_x, node_y, cols, rows)
         for adj in adjacents:
-            adjX = adj[0]
-            adjY = adj[1]
+            adj_x = adj[0]
+            adj_y = adj[1]
 
-            new_path_length = paths[nodeX][nodeY] + weights[adjX][adjY]
+            new_path_length = paths[node_x][node_y] + weights[adj_x][adj_y]
 
-            if visited_matrix[adjX][adjY] != 1:
+            if visited_matrix[adj_x][adj_y] != 1:
                 # avoid padding a node to the queue if already present - use a set for performance reasons
-                queueSet = set(queue)
-                if adj not in queueSet:
+                queue_set = set(queue)
+                if adj not in queue_set:
                     queue.append(adj)
-            if new_path_length < paths[adjX][adjY]:
-                paths[adjX][adjY] = new_path_length
+            if new_path_length < paths[adj_x][adj_y]:
+                paths[adj_x][adj_y] = new_path_length
                 queue.append(adj)
 
     return paths
@@ -113,94 +112,80 @@ def find_removable_walls(paths_matrix):
     number_cols = len(paths_matrix[0])
     number_rows = len(paths_matrix)
 
-    removableWalls = []
+    removable_walls = []
 
     for i in range(0, number_rows):
         for j in range(0, number_cols):
             # walls that can be removed in a useful way to produce a shorter paths
-            # have 2 charateristics: path length between 1000 and 2000 and are adjacent to a cell which is 
-            # part of an existing path (checked below when we do the paths_matrix[adjX][adjY] < 1000 comparison)
+            # have 2 characteristics: path length between 1000 and 2000 and are adjacent to a cell which is
+            # part of an existing path (checked below when we do the paths_matrix[adj_x][adj_y] < 1000 comparison)
 
-            if 1000 < paths_matrix[i][j] < 2000:  
+            if 1000 < paths_matrix[i][j] < 2000:
                 adjacents = find_adjacents_of_a_cell(i, j, number_cols, number_rows)
                 counter = 0
 
                 for adj in adjacents:
-                    adjX = adj[0]
-                    adjY = adj[1]
+                    adj_x = adj[0]
+                    adj_y = adj[1]
 
-                    if paths_matrix[adjX][adjY] < 1000:
+                    if paths_matrix[adj_x][adj_y] < 1000:
                         counter += 1
                         if counter > 0:
-                            removableWalls.append((i, j))
+                            removable_walls.append((i, j))
                             break
 
-    return removableWalls
+    return removable_walls
+
 
 def solution(maze):
-
     paths_matrix = BFS(maze)
 
     number_cols = len(paths_matrix[0])
     number_rows = len(paths_matrix)
 
-    # the lenght of the path is stored in the bottom-right corner
-    shortestPath = paths_matrix[number_rows - 1][number_cols - 1]
+    # the length of the path is stored in the bottom-right corner
+    shortest_path = paths_matrix[number_rows - 1][number_cols - 1]
 
     # the shortest possible path won't be shorter than the longest dimension (number or rows or columns)
     # if we find something as short as that there's no need to search further
-    bestAbsolutePath = number_rows
+    best_absolute_path = number_rows
     if number_cols > number_rows:
-        bestAbsolutePath = number_cols
+        best_absolute_path = number_cols
 
-    if shortestPath == bestAbsolutePath:
-        return shortestPath
+    if shortest_path == best_absolute_path:
+        return shortest_path
 
     # can we remove some walls to search for a shorter path?
-    removableWalls = find_removable_walls(paths_matrix)
+    removable_walls = find_removable_walls(paths_matrix)
 
-    for wall in removableWalls:
-        wallX = wall[0]
-        wallY = wall[1]
+    for wall in removable_walls:
+        wall_x = wall[0]
+        wall_y = wall[1]
 
-        maze[wallX][wallY] = 0 # remove that wall in the original maze
+        maze[wall_x][wall_y] = 0  # remove that wall in the original maze
 
         paths_matrix = BFS(maze)
 
         new_shortest_path_length = paths_matrix[number_rows - 1][number_cols - 1]
 
-        if new_shortest_path_length < shortestPath:
-            shortestPath = new_shortest_path_length
+        if new_shortest_path_length < shortest_path:
+            shortest_path = new_shortest_path_length
 
-        if shortestPath == bestAbsolutePath: # again, in this case we have finished, can't do better anyway
-            return shortestPath
+        if shortest_path == best_absolute_path:  # again, in this case we have finished, can't do better anyway
+            return shortest_path
 
-        maze[wallX][wallY] = 1 # restore the wall before moving to the next one
+        maze[wall_x][wall_y] = 1  # restore the wall before moving to the next one
 
-    return shortestPath
-
+    return shortest_path
 
 
 # the  following part is not required for the Google challenge
-def milliseconds():
-    return int(round(time.time() * 1000))
 
-def main():
-    start_time = milliseconds()
-    length = solution(maze4)
-
-    end_time = milliseconds()
-
-    print("Route length", length, "in", end_time - start_time, "milliseconds.")
-
-main()
-
-
-
-#Test mazes 
-maze = [[0, 1, 1, 0], [0, 0, 0, 1], [1, 1, 0, 0], [1, 1, 1, 0]] 
+# Test mazes
+maze0 = [[0, 1, 1, 0], [0, 0, 0, 1], [1, 1, 0, 0], [1, 1, 1, 0]]
 maze1 = [[0, 1, 1], [1, 0, 0], [1, 1, 0]]
-maze2 = [[0, 0, 0, 0, 0, 0], [1, 1, 1, 1, 1, 0], [0, 0, 0, 0, 0, 0], [0, 1, 1, 1, 1, 1], [0, 1, 1, 1, 1, 1], [0, 0, 0, 0, 0, 0]]
+maze2 = [[0, 0, 0, 0, 0, 0], [1, 1, 1, 1, 1, 0], [0, 0, 0, 0, 0, 0], [0, 1, 1, 1, 1, 1], [0, 1, 1, 1, 1, 1],
+         [0, 0, 0, 0, 0, 0]]
 maze3 = [[0, 0, 0], [1, 1, 0], [0, 0, 0], [0, 1, 1], [0, 0, 0]]
 maze4 = [[0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
          [1, 1, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0],
@@ -222,3 +207,18 @@ maze4 = [[0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
          [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
          [0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0],
          [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]]
+
+def milliseconds():
+    return int(round(time.time() * 1000))
+
+
+def main():
+    start_time = milliseconds()
+    length = solution(maze4)
+
+    end_time = milliseconds()
+
+    print("Route length", length, "in", end_time - start_time, "milliseconds.")
+
+
+main()
